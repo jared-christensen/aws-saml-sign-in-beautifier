e5b19eab0004b4f66e4ec2f1994eefe1fb0acbe7
Jared Christensen <jared.christensen@dhigroupinc.com>
2025-05-04 00:07:01 -0500

    feat: enhance grid layout responsiveness and adjust card styling for improved visual consistency

--- src/lib/set-grid-columns.ts
+++ src/lib/set-grid-columns.ts
@@ -3,5 +3,8 @@ import type { Options } from "~schema/options-schema";
 export function setGridColumns(options?: Options) {
   if (options?.gridColumns) {
     document.documentElement.style.setProperty("--grid-columns", options.gridColumns.toString());
+    if (Number(options?.gridColumns) <= 2) {
+      document.documentElement.style.setProperty("--grid-container-width", "740px");
+    }
   }
 }
